257e09daae4cafe44afe49a4b31579261933eeb7
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2020-10-02 17:18:20 +0300

    Took into account the Muslim's comments #3

--- CountWorkTime_bot.py
+++ CountWorkTime_bot.py
@@ -2,13 +2,12 @@ import cv2
 
 from utils import get_frame, start_caption_frame
 
-face_counter = 0
-
 
 def main():
     face_cascade, videostream_for_caption = start_caption_frame()
     while True:
-        frame = get_frame(face_cascade, videostream_for_caption)
+        frame, face_increment = get_frame(face_cascade, videostream_for_caption)
+        face_counter += face_increment
         cv2.imshow('frame', frame)
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
--- utils.py
+++ utils.py
@@ -14,15 +14,23 @@ path_to_face_cascade = os.path.join(
 
 
 def start_caption_frame():
-    face_cascade = cv2.CascadeClassifier(path_to_face_cascade)
+    try:
+        face_cascade = cv2.CascadeClassifier(path_to_face_cascade)
+    except FileNotFoundError:
+        print('Файл с шаблонами не найден')
     video_capture = cv2.VideoCapture(0)
     return face_cascade, video_capture
 
 
 def get_frame(face_cascade, videostream_for_caption):
     ret, frame = videostream_for_caption.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    except cv2.error as e:
+        if e.err == "!_src.empty()":
+            print('Видеопоток не найден, проверьте подключение камеры')
+            return False, 0
     for (start_coord_x, start_coord_y, width, hieght) in faces:
         print(start_coord_x, start_coord_y, width, hieght)
         end_coordinate_x = start_coord_x + width
@@ -34,4 +42,8 @@ def get_frame(face_cascade, videostream_for_caption):
         roi_end_point = (end_coordinate_x, end_coordinate_y)
 
         cv2.rectangle(frame, roi_start_point, roi_end_point, roi_color, roi_stroke)
-    return frame
+    if faces != ():
+        face_increment = 1
+    else:
+        face_increment = 0
+    return frame, face_increment
