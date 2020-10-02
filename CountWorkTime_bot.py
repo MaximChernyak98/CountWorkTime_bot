570596bb32e98444667e1c81787d15ae523d2267
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2020-10-02 20:15:32 +0300

    implemented presence / absence detection for 10 seconds

--- CountWorkTime_bot.py
+++ CountWorkTime_bot.py
@@ -1,15 +1,18 @@
 import cv2
 
-from utils import get_frame, start_caption_frame
+from utils import search_faces_in_frames, start_caption_frame, is_man_at_work
 
 
 def main():
     face_cascade, videostream_for_caption = start_caption_frame()
+
+    is_man_at_work_b = False
+    number_of_hits_to_start = 0
     while True:
-        frame, face_increment = get_frame(face_cascade, videostream_for_caption)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
+        number_of_face_occurrences = search_faces_in_frames(face_cascade, videostream_for_caption)
+        is_man_at_work_b, result_number_of_hits = is_man_at_work(number_of_face_occurrences, number_of_hits_to_start)
+        number_of_hits_to_start = result_number_of_hits
+        print(f'{result_number_of_hits} - человек на работе? {is_man_at_work_b}')
     videostream_for_caption.release()
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-
+import datetime
 import cv2
 
 current_dir = os.path.dirname(__file__)
@@ -22,24 +22,44 @@ def start_caption_frame():
     return face_cascade, video_capture
 
 
-def get_frame(face_cascade, videostream_for_caption):
-    ret, frame = videostream_for_caption.read()
-    try:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    except cv2.error as e:
-        if e.err == "!_src.empty()":
-            print('Видеопоток не найден, проверьте подключение камеры')
-            return False, 0
-    for (start_coord_x, start_coord_y, width, hieght) in faces:
-        print(start_coord_x, start_coord_y, width, hieght)
-        end_coordinate_x = start_coord_x + width
-        end_coordinate_y = start_coord_y + hieght
+def search_faces_in_frames(face_cascade, videostream_for_caption):
+    number_of_face_occurrences = 0
+    end_capture_time = datetime.datetime.now() + datetime.timedelta(seconds=1)
+    while datetime.datetime.now() < end_capture_time:
+        ret, frame = videostream_for_caption.read()
+        try:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        except cv2.error as e:
+            if e.err == "!_src.empty()":
+                print('Видеопоток не найден, проверьте подключение камеры')
+                return False, 0
+        for (start_coord_x, start_coord_y, width, hieght) in faces:
+            print(start_coord_x, start_coord_y, width, hieght)
+            end_coordinate_x = start_coord_x + width
+            end_coordinate_y = start_coord_y + hieght
 
-        roi_color = (255, 0, 0)  # BGR
-        roi_stroke = 2
-        roi_start_point = (start_coord_x, start_coord_y)
-        roi_end_point = (end_coordinate_x, end_coordinate_y)
+            roi_color = (255, 0, 0)  # BGR
+            roi_stroke = 2
+            roi_start_point = (start_coord_x, start_coord_y)
+            roi_end_point = (end_coordinate_x, end_coordinate_y)
 
-        cv2.rectangle(frame, roi_start_point, roi_end_point, roi_color, roi_stroke)
-    return frame
+            cv2.rectangle(frame, roi_start_point, roi_end_point, roi_color, roi_stroke)
+        if faces != ():
+            number_of_face_occurrences += 1
+    return number_of_face_occurrences
+
+
+def is_man_at_work(number_of_face_occurrences, number_of_hits_to_start):
+    if number_of_face_occurrences != 0:
+        number_of_hits_to_start += 1
+        if number_of_hits_to_start > 10:
+            number_of_hits_to_start = 10
+            return True, number_of_hits_to_start
+        return False, number_of_hits_to_start
+    else:
+        number_of_hits_to_start -= 1
+        if number_of_hits_to_start < 0:
+            number_of_hits_to_start = 0
+            return False, number_of_hits_to_start
+        return True, number_of_hits_to_start
